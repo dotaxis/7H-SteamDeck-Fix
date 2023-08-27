1436ccbe37f4d09c5bbc04578fd4204b3f4482ca
Chase Taylor <x8@live.ca>
2023-08-26 23:19:11 -0600

    Adjust console output
    Hide protontricks output because that isn't broken anymore
    
    Changed some wording
    
    Improve readability

--- 7HFix.sh
+++ 7HFix.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+echo -e "7H-SteamDeck-Fix v1.1\n"
 shopt -s expand_aliases
 alias protontricks='flatpak run com.github.Matoking.protontricks'
 
 DEFAULT_7TH_HEAVEN_DIRECTORY="7th Heaven"
 DEFAULT_7TH_HEAVEN_APP_NAME="7th Heaven"
 
+
 # Install protontricks and fix for SD cards
+echo "Installing Protontricks..."
 flatpak --system install com.github.Matoking.protontricks -y
 flatpak override --user --filesystem=/run/media/mmcblk0p1 com.github.Matoking.protontricks
+echo
+
 
 # Check for -c flag
 copy_ff7=0
@@ -18,6 +23,7 @@ while getopts "c" opt; do
     *) copy_ff7=0 ;;
   esac
 done
+
 
 # Download 7th Heaven from Github
 downloadDependency() {
@@ -39,9 +45,10 @@ downloadDependency() {
   fi
   eval "${RETURN_VARIABLE}=\"$FILENAME\""
 }
+echo "Downloading 7th Heaven..."
 downloadDependency "tsunamods-codes/7th-Heaven" "*.exe" SEVENHEAVEN
+echo
 
-zenity --width=500 --info --text="Make sure you have Final Fantasy VII installed on Steam.\nPress OK when you are ready to continue."
 
 # Detect install location
 FF7_LOCATION="${HOME}/.local/share/Steam/steamapps/common/FINAL FANTASY VII"
@@ -49,13 +56,15 @@ FF7_LOCATION="${HOME}/.local/share/Steam/steamapps/common/FINAL FANTASY VII"
 [ ! -d "$FF7_LOCATION" ] && FF7_LOCATION="/run/media/mmcblk0p1/steamapps/common/FINAL FANTASY VII"
 [ ! -d "$FF7_LOCATION" ] && FF7_LOCATION=$(zenity --width=500 --entry \
   --title="FF7 Location" \
-  --text="Enter the location of your FINAL FANTASY VII folder." \
+  --text="Couldn't detect a FINAL FANTASY VII installation on this device.\n
+  Enter the path to your installation." \
   --entry-text="${HOME}/.local/share/Steam/steamapps/common/FINAL FANTASY VII")
+
 
 # Instructions popup
 zenity --width=500 --info \
---title="Installation" \
---text="Time to install 7th Heaven Canary and configure it for the Steam Deck!\n
+--title="Instructions" \
+--text="Time to install 7th Heaven and configure it for the Steam Deck!\n
 1. Add a game as a \"Non-Steam Game\"\n
 2. Select \""$(pwd)/$SEVENHEAVEN"\"\n
 3. Name it whatever you want but remember that name for later
@@ -69,6 +78,7 @@ zenity --width=500 --info \
 
 zenity --width=500 --info --text="Wait for installation to complete. Close the wizard.\nPress OK when you are ready to continue."
 
+
 # Protontricks APP_ID finder
 zenity --width=500 --question --text="Is your Non-Steam Game named: \"${DEFAULT_7TH_HEAVEN_APP_NAME}\"?"
 if [ $? -eq 0 ]; then
@@ -79,9 +89,10 @@ else
   --text="What is your Non-Steam Game named?" \
   --entry-text="$DEFAULT_7TH_HEAVEN_APP_NAME")
 fi
-APP_ID=$(protontricks -s $SEVENTH_HEAVEN_APP_NAME | grep -P "Non-Steam shortcut: $SEVENTH_HEAVEN_APP_NAME \([0-9]+\)" | grep -Po "(?<=\()[0-9].+(?=\))")
+APP_ID=$(protontricks -s $SEVENTH_HEAVEN_APP_NAME 2> /dev/null | grep -P "Non-Steam shortcut: $SEVENTH_HEAVEN_APP_NAME \([0-9]+\)" | grep -Po "(?<=\()[0-9].+(?=\))")
 [[ ! $APP_ID =~ ^[0-9]+$ ]] && zenity --width=500 --error --text="APP_ID was not found for \"$SEVENTH_HEAVEN_APP_NAME\". Make sure the name entered matches and retry." && exit
 WINEPATH="${HOME}/.steam/steam/steamapps/compatdata/$APP_ID/pfx"
+
 
 # Option to move 7H to SD card when using -c flag
 if [ "$copy_ff7" -eq 1 ]; then
@@ -98,12 +109,15 @@ if [ "$copy_ff7" -eq 1 ]; then
   fi
 fi
 
+
 # No-CD fix
 mkdir -p "$WINEPATH/drive_c"
 echo "FF7DISC1" > "$WINEPATH/drive_c/.windows-label"
 echo "44000000" > "$WINEPATH/drive_c/.windows-serial"
 [ -d "$WINEPATH/drive_c/FF7" ] && rm -r "$WINEPATH/drive_c/FF7"
 
+
+# Copy or Symlink?
 if [ "$copy_ff7" -eq 1 ]; then
   # Copy FF7 directory to C:\FF7
   rsync -av --progress "$FF7_LOCATION/" "$WINEPATH/drive_c/FF7" |
@@ -115,6 +129,7 @@ else
   ln -fs "$FF7_LOCATION/" "$WINEPATH/drive_c/FF7"
 fi
 
+
 # Copy settings and patched exe
 mkdir -p $WINEPATH/drive_c/FF7/mods/{"7th Heaven",textures}
 FULL_PATH="$WINEPATH/drive_c/$DEFAULT_7TH_HEAVEN_DIRECTORY"
@@ -123,17 +138,20 @@ mkdir -p "$FULL_PATH/7thWorkshop/"
 cp -f deps/settings.xml "$FULL_PATH/7thWorkshop/"
 cp -f "$FULL_PATH/Resources/FF7_1.02_Eng_Patch/ff7.exe" "$WINEPATH/drive_c/FF7/ff7.exe"
 
+
 # Change target of Steam shortcut
 for SHORTCUTSFILE in ${HOME}/.steam/steam/userdata/*/config/shortcuts.vdf ; do
   sed -i "s:$(pwd)/${SEVENHEAVEN}:${FULL_PATH}/7th Heaven.exe:" $SHORTCUTSFILE
   sed -i "s:$(pwd):${FULL_PATH}:" $SHORTCUTSFILE
 done
 
+
 # Fix updater
 [ -f "$WINEPATH/drive_c/windows/syswow64/robocopy.exe" ] && rm "$WINEPATH/drive_c/windows/syswow64/robocopy.exe"
 [ -f "$WINEPATH/drive_c/windows/system32/robocopy.exe" ] && rm "$WINEPATH/drive_c/windows/system32/robocopy.exe"
 cp -f deps/robocopy.bat "$WINEPATH/drive_c/windows/system32/"
 cp -f deps/timeout.exe "$WINEPATH/drive_c/windows/system32/"
+
 
 # Option to copy saves
 zenity --width=500 --question --title="Save Files" --text="Do you want to copy your save files from Vanilla FF7?"
@@ -146,21 +164,33 @@ if [[ $? -eq 0 ]]; then
   cd "$SAVES_FOLDER"
   for file in *".ff7" ; do
     cp -f -- "$file" "$WINEPATH/drive_c/FF7/save/$file"
+    echo "Copied save: $file"
   done
 fi
 
+
 # Dinput.dll fix
+echo
+echo "Patching 7th Heaven with Protontricks. Please wait..."
 [ -f "$WINEPATH/drive_c/windows/syswow64/dinput.dll" ] && rm "$WINEPATH/drive_c/windows/syswow64/dinput.dll"
-protontricks $APP_ID dinput
+protontricks $APP_ID dinput &> /dev/null
+echo -e "Done!"
+
 
 # Restart Steam
+echo
+echo "Restarting Steam..."
 kill $(ps aux | grep '[s]team -steamdeck' | awk '{print $2}')
 sleep 10
-steam > /dev/null 2>&1 & disown
+nohup steam &> /dev/null &
 
+
+# Final popup
 zenity --width=500 --info \
 --title="Done!" \
---text="7th Heaven Canary has been successfully installed!\n
+--text="7th Heaven has been successfully installed!\n
 <b>******* IMPORTANT *******
 ⚠️ CLICK SAVE THE FIRST TIME YOU OPEN 7TH HEAVEN
 ⚠️ RUN THE GAME ONCE IN ORDER TO INSTALL FFNx</b>"
+echo "Success! You can close this window."
+kill -9 $PPID
